46ee2da7a837e0cf0413fe83bf54ed0147088c77
EMILYHLANE <embem80@gmail.com>
2018-10-23 10:35:10 -0700

    adds nav slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,19 @@
 //alert("javascript is working!");
 
+function showHideNav() {
+	$('.nav').toggleClass('small-nav-closed');
+	$('.nav').toggleClass('small-nav-open');
+	$('.fa-times').toggleClass('close-icon');
+	$('.fa-times').toggleClass('hide');
+};
+
+$('.hamburger-icon').on('click', showHideNav);
+$('.fa-times').on('click', showHideNav);
+
+
+
+
+
+
+
+
