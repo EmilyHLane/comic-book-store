94b171e2859cd5c125b830b9675773e9cdd6afe5
EMILYHLANE <embem80@gmail.com>
2018-10-23 13:26:27 -0700

    adds validation with error

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,26 @@ $('.hamburger-icon').on('click', showHideNav);
 $('.fa-times').on('click', showHideNav);
 
 
+$("#mc-embedded-subscribe-form").on('submit', function(e) {
+	e.preventDefault();
+	var firstNameInput = $("#mce-FNAME").val();
+	var lastNameInput = $("#mce-LNAME").val();
+	var emailInput = $("#mce-EMAIL").val();
+
+	if (firstNameInput === "" || lastNameInput === "" || emailInput === "") {
+		$(".error-msg").append("<p>One of the required fields is empty</p>");
+	} else if (firstNameInput.length < 2) {
+		$(".error-msg").append("<p>First name must have at least 2 letters</p>");
+	} else if (lastNameInput.length < 2) {
+		$(".error-msg").append("<p>Last name must have at least 2 letters</p>");
+	} else if (emailInput.includes("@") === false || emailInput.includes(".") === false) {
+		$(".error-msg").append("<p>email must contain @ and . </p>");
+	} else {
+		$("#mc-embedded-subscribe-form").submit();
+	}
+});
+
+
 
 
 
